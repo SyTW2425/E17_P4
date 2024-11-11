33c8031c028f41edcb0e90fd002cf65aa72e3173
JoelULL <alu0100987584@ull.edu.es>
2024-11-11 17:05:42 +0000

    starting defining some components. this one is about a list of transations

---
+++ MyInventoryHub/src/app/components/inventory/inventory-transaction-list/inventory-transaction-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { InventoryTransactionService } from '../../../services/inventory-transaction.service';
+import { InventoryTransaction } from '../../../models/inventoryTransaction.model';
+
+@Component({
+  selector: 'app-inventory-transaction-list',
+  templateUrl: './inventory-transaction-list.component.html',
+  styleUrls: ['./inventory-transaction-list.component.css']
+})
+export class InventoryTransactionListComponent implements OnInit {
+  transactions: InventoryTransaction[] = [];
+
+  constructor(private transactionService: InventoryTransactionService) {}
+
+  ngOnInit(): void {
+    this.transactions = this.transactionService.getAllTransactions();
+  }
+}
