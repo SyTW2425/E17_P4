53d7e938909d85d3fb7b767e7fe9d18bc6f1d750
JoelULL <alu0100987584@ull.edu.es>
2024-11-11 13:09:27 +0000

    removed useless comments

--- MyInventoryHub/src/app/services/order.service.spec.ts
+++ MyInventoryHub/src/app/services/order.service.spec.ts
@@ -1,20 +1,3 @@
-/**import { TestBed } from '@angular/core/testing';
-
-import { OrderService } from './order.service';
-
-describe('OrderService', () => {
-  let service: OrderService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrderService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
-**/
 import { TestBed } from '@angular/core/testing';
 import { OrderService } from './order.service';
 import { Order, OrderItem } from '../models/order.model';
