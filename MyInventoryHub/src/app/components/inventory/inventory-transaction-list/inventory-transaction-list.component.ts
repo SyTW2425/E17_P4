d5503ed40d3d57ef4fe7f3a5b06b0bf3af55b822
JoelULL <alu0100987584@ull.edu.es>
2024-11-11 17:25:57 +0000

    added some comments for context

--- MyInventoryHub/src/app/components/inventory/inventory-transaction-list/inventory-transaction-list.component.ts
+++ MyInventoryHub/src/app/components/inventory/inventory-transaction-list/inventory-transaction-list.component.ts
@@ -1,3 +1,8 @@
+/**
+ * ngOnInit is used to initialize the component after Angular has fully set up the component and injected dependencies.
+ * It is a safe place to perform tasks like fetching data or setting up initial state, ensuring that the component 
+ * is ready for interaction and rendering.
+*/
 import { Component, OnInit } from '@angular/core';
 import { InventoryTransactionService } from '../../../services/inventory-transaction.service';
 import { InventoryTransaction } from '../../../models/inventoryTransaction.model';
@@ -9,6 +14,10 @@ import { InventoryTransaction } from '../../../models/inventoryTransaction.model
   templateUrl: './inventory-transaction-list.component.html',
   styleUrls: ['./inventory-transaction-list.component.css']
 })
+/**
+ * Adds a property that saves a list of all the inventory transactions 
+ * obtained by the service.
+ */
 export class InventoryTransactionListComponent implements OnInit {
   transactions: InventoryTransaction[] = [];
 
