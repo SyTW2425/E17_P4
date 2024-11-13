58533f228b4f9e1d675dc49181634a4a2bcb208a
JoelULL <alu0100987584@ull.edu.es>
2024-11-13 17:49:12 +0000

    executed ng generate component inventorytransaction form

---
+++ MyInventoryHub/src/app/components/inventory/inventory-transaction-form/inventory-transaction-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-transaction-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-transaction-form.component.html',
+  styleUrl: './inventory-transaction-form.component.css'
+})
+export class InventoryTransactionFormComponent {
+
+}
