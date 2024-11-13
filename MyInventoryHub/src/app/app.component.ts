5378d1c1427c6ea9fb2e61b87c6bfc129e0c84ff
JoelULL <alu0100987584@ull.edu.es>
2024-11-13 17:47:03 +0000

    added some test code for testing a component. Removing later for a spec

--- MyInventoryHub/src/app/app.component.ts
+++ MyInventoryHub/src/app/app.component.ts
@@ -11,3 +11,32 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'MyInventoryHub';
 }
+
+/** 
+import { Component } from '@angular/core';
+import { InventoryTransactionDetailComponent } from './components/inventory/inventory-transaction-detail/inventory-transaction-detail.component';
+import { InventoryTransaction } from './models/inventoryTransaction.model';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [InventoryTransactionDetailComponent],
+  template: `
+    <div style="padding: 20px;">
+      <app-inventory-transaction-detail [transaction]="testTransaction"></app-inventory-transaction-detail>
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Objeto de prueba
+  testTransaction: InventoryTransaction = {
+    id: 1,
+    productId: 101,
+    transactionType: 'IN',
+    quantity: 50,
+    transactionDate: new Date(),
+    warehouseId: 1
+  };
+}
+*/
