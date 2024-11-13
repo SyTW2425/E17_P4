ee77c1c3ae039a1287fd7c6c16639d9528ac536c
JoelULL <alu0100987584@ull.edu.es>
2024-11-13 17:58:22 +0000

    added a form component. not tested yet

--- MyInventoryHub/src/app/components/inventory/inventory-transaction-form/inventory-transaction-form.component.ts
+++ MyInventoryHub/src/app/components/inventory/inventory-transaction-form/inventory-transaction-form.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InventoryTransaction } from '../../../models/inventoryTransaction.model';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory-transaction-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './inventory-transaction-form.component.html',
-  styleUrl: './inventory-transaction-form.component.css'
+  styleUrls: ['./inventory-transaction-form.component.css']
 })
 export class InventoryTransactionFormComponent {
+  @Input() transaction: InventoryTransaction | null = null;
+  @Output() formSubmit = new EventEmitter<InventoryTransaction>();
+  transactionForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.transactionForm = this.fb.group({
+      id: [null, Validators.required],
+      productId: [null, Validators.required],
+      transactionType: ['IN', Validators.required],
+      quantity: [null, [Validators.required, Validators.min(1)]],
+      transactionDate: [new Date(), Validators.required],
+      warehouseId: [null, Validators.required],
+      notes: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.transaction) {
+      this.transactionForm.patchValue(this.transaction);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.transactionForm.valid) {
+      this.formSubmit.emit(this.transactionForm.value);
+    }
+  }
 }
