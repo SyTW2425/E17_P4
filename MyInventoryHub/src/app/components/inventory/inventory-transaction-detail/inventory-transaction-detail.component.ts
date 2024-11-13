88852a830d6478db569bb0bd28dd27e414f69cde
JoelULL <alu0100987584@ull.edu.es>
2024-11-13 17:08:09 +0000

    executed ng generate component inventory-transaction-detail

---
+++ MyInventoryHub/src/app/components/inventory/inventory-transaction-detail/inventory-transaction-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-transaction-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-transaction-detail.component.html',
+  styleUrl: './inventory-transaction-detail.component.css'
+})
+export class InventoryTransactionDetailComponent {
+
+}
