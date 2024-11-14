7f647534e7bb58c8f4a2d16e5bdd186f428d3d08
JoelULL <alu0100987584@ull.edu.es>
2024-11-14 19:29:52 +0000

    more changes on the first concept of login. asset dir enabled

--- MyInventoryHub/src/app/components/user-managment/login/login.component.ts
+++ MyInventoryHub/src/app/components/user-managment/login/login.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports:[CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
