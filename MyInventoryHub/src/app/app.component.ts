ac6c9893498474d83838b788014e74e870742f65
JoelULL <alu0100987584@ull.edu.es>
2024-11-14 17:54:02 +0000

    added some logic to order-list

--- MyInventoryHub/src/app/app.component.ts
+++ MyInventoryHub/src/app/app.component.ts
@@ -40,3 +40,52 @@ export class AppComponent {
   };
 }
 */
+/** 
+ * testeando order details
+import { Component } from '@angular/core';
+import { Order } from './models/order.model';
+import { OrderListComponent } from '../app/components/orders/order-list/order-list.component';
+import { OrderDetailComponent } from '../app/components/orders/order-detail/order-detail.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-order-list [orders]="orders" (selectOrder)="onOrderSelected($event)"></app-order-list>
+    <app-order-detail *ngIf="selectedOrder" [order]="selectedOrder!"></app-order-detail>
+  `,
+  standalone: true,
+  imports: [CommonModule, OrderListComponent, OrderDetailComponent]
+})
+export class AppComponent {
+  orders: Order[] = [
+    {
+      id: 1,
+      supplierId: 123,
+      orderDate: new Date(),
+      deliveryDate: new Date(),
+      status: 'PENDING',
+      orderItems: [
+        { productId: 101, quantity: 2, unitPrice: 15.0 },
+        { productId: 102, quantity: 1, unitPrice: 30.0 }
+      ]
+    },
+    {
+      id: 2,
+      supplierId: 456,
+      orderDate: new Date(),
+      status: 'SHIPPED',
+      orderItems: [
+        { productId: 103, quantity: 3, unitPrice: 20.0 },
+        { productId: 104, quantity: 4, unitPrice: 25.0 }
+      ]
+    }
+  ];
+
+  selectedOrder?: Order;
+
+  onOrderSelected(order: Order) {
+    this.selectedOrder = order;
+  }
+}
+*/
--- MyInventoryHub/src/app/components/orders/order-list/order-list.component.ts
+++ MyInventoryHub/src/app/components/orders/order-list/order-list.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Order } from '../../../models/order.model';
 
 @Component({
   selector: 'app-order-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-list.component.html',
-  styleUrl: './order-list.component.css'
+  styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent {
+  @Input() orders: Order[] = []; // Recibe la lista de órdenes desde el componente padre
+  @Output() selectOrder = new EventEmitter<Order>(); // Emite un evento cuando se selecciona una orden
 
+  onSelect(order: Order) {
+    this.selectOrder.emit(order);
+  }
 }
