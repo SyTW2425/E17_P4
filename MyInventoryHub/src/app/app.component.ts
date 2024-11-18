dfd1239e734fd6c17c00bcd8ee602bd4e030b89d
Ancor - Portátil <alu0101327679@ull.edu.es>
2024-11-18 18:30:46 +0000

    Error fixed

--- MyInventoryHub/src/app/app.component.ts
+++ MyInventoryHub/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { BatchListComponent } from './components/batch/batch-list/batch-list.component';
 import { ProductManagerComponent } from './components/product/product-manager/product-manager.component';
 import { SupplierManagerComponent }from './components/supplier-manager/supplier-manager.component'
-import { SignInComponent } from './components/user/user-sign-in/user-sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BatchListComponent, ProductManagerComponent, SupplierManagerComponent, SignInComponent],
+  imports: [RouterOutlet, BatchListComponent, ProductManagerComponent, SupplierManagerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
