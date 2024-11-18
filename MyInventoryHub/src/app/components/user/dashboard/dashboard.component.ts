8c649390bb31c7ada6fccfa8a2b3c17672623972
Ancor - Portátil <alu0101327679@ull.edu.es>
2024-11-18 18:27:51 +0000

    FIX - features implemetned in wrong place

--- MyInventoryHub/src/app/components/user/dashboard/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard',
-  template: `<h1>¡Bienvenido al Dashboard!</h1>`,
-  styles: [`h1 { text-align: center; margin-top: 50px; }`]
-})
-export class DashboardComponent {}
