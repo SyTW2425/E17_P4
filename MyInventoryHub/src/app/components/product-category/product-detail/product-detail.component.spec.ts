9dfde68b5effc185696ff6546e26018fd46f4d8a
JonathanMartinezPerez <alu0101254098@ull.edu.es>
2024-11-19 14:00:56 +0000

    merge fixed

--- MyInventoryHub/src/app/components/product-category/product-detail/product-detail.component.spec.ts
+++ MyInventoryHub/src/app/components/product-category/product-detail/product-detail.component.spec.ts
@@ -1,16 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductCategoryDetailComponent } from './product-detail.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('ProductDetailComponent', () => {
+describe('ProductCategoryDetailComponent', () => {
   let component: ProductCategoryDetailComponent;
   let fixture: ComponentFixture<ProductCategoryDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductCategoryDetailComponent]
-    })
-    .compileComponents();
+      imports: [ProductCategoryDetailComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '1', // Devuelve un valor de prueba para el parámetro 'id'
+              },
+            },
+            
+            params: of({ id: '1' }),
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductCategoryDetailComponent);
     component = fixture.componentInstance;
