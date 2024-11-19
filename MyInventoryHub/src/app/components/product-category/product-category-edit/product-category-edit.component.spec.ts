0a75851b9fe89e6ecf3617b29cdc5efb979066aa
JoelULL <alu0100987584@ull.edu.es>
2024-11-19 13:58:19 +0000

    fixed some test problems. hotfix

--- MyInventoryHub/src/app/components/product-category/product-category-edit/product-category-edit.component.spec.ts
+++ MyInventoryHub/src/app/components/product-category/product-category-edit/product-category-edit.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductCategoryEditComponent } from './product-category-edit.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
 
 describe('ProductCategoryEditComponent', () => {
   let component: ProductCategoryEditComponent;
@@ -8,9 +10,27 @@ describe('ProductCategoryEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductCategoryEditComponent]
-    })
-    .compileComponents();
+      imports: [ProductCategoryEditComponent],
+      providers: [
+        FormBuilder,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '1', // Mock del parámetro 'id'
+              },
+            },
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'), // Mock de `navigate`
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductCategoryEditComponent);
     component = fixture.componentInstance;
