18b9f95c404782aa061515ea2fc4a5b7203ed09d
JonathanMartinezPerez <alu0101254098@ull.edu.es>
2024-11-20 22:32:45 +0000

    Fist iteration sign up

--- MyInventoryHub/src/app/app.component.ts
+++ MyInventoryHub/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SupplierManagerComponent }from './components/supplier/supplier.component'
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, SupplierManagerComponent],
+  imports: [RouterOutlet, SupplierManagerComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ MyInventoryHub/src/app/components/register/register.component.ts
@@ -0,0 +1,54 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
+import { first } from 'rxjs';
+
+@Component({
+    selector: 'app-register',
+    standalone: true,
+    imports: [
+      ReactiveFormsModule,
+      RouterModule,
+      NgIf,
+      NgClass
+    ],
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
+  })
+  export class RegisterComponent {
+    form: FormGroup;
+    submitted = false;
+    loading = false;
+  
+    constructor(private formBuilder: FormBuilder) {
+      // Initialize the form
+      this.form = this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      });
+    }
+  
+    get f() {
+      return this.form.controls;
+    }
+  
+    onSubmit() {
+      this.submitted = true;
+  
+      // Stop if form is invalid
+      if (this.form.invalid) {
+        return;
+      }
+  
+      this.loading = true;
+  
+      // Simulate an API call
+      setTimeout(() => {
+        this.loading = false;
+        alert('User registered successfully!');
+      }, 2000);
+    }
+  }
