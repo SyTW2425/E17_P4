ef8ba5b54c9a678fc670df3ea9d1d2703b5c55e9
JonathanMartinezPerez <alu0101254098@ull.edu.es>
2024-11-27 12:02:16 +0000

    dashboard template

--- MyInventoryHub/src/app/app.routes.ts
+++ MyInventoryHub/src/app/app.routes.ts
@@ -11,12 +11,32 @@ import { AboutComponent } from './components/pages/landing-page/about/about.comp
 import { ContactComponent } from './components/pages/landing-page/contact/contact.component';
 import { FaqComponent } from './components/pages/landing-page/faq/faq.component';
 import { IndexComponent } from './components/pages/landing-page/index/index.component';
+import LayoutComponent from './shared/components/layout/layout.component';
+import DashboardComponent from './business/dashboard/dashboard.component';
+import ProfileComponent from './business/profile/profile.component';
+import TablesComponent from './business/tables/tables.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FaqComponent },
+  { path: 'dashboard', component: LayoutComponent,
+    children: [
+        {
+            path: '',
+            component: DashboardComponent
+        },
+        {
+            path: 'profile',
+            component: ProfileComponent
+        },
+        {
+            path: 'tables',
+            component: TablesComponent
+        }
+    ]
+   },
   { path: '**', redirectTo: '' } // Redirección en caso de ruta no encontrada
 ];
 
---
+++ MyInventoryHub/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/business/**/**/*.{html,ts}",
+    "./src/app/shared/**/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
